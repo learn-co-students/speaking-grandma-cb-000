7e8ce747c5caf5a7455aa57d55c1b36bc2ca0c7e
Alec German <alec_german@yahoo.com>
2018-01-31 03:08:41 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string == string.uppercase
+  if string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+  else
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
