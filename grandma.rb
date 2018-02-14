7783ec9aec3187b5f315aa15048e2e06dd7164de
Jay Fleming <jacobmfleming@gmail.com>
2018-02-14 22:01:34 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,12 @@
 
 def speak_to_grandma(phrase)
 
-  if phrase.upcase != phrase
-    puts "HUH?! SPEAK UP, SONNY!"
+  if phrase.upcase() != phrase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
 
